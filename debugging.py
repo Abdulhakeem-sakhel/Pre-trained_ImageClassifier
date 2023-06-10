c5cb26a152219cf485f1280e74fdb7acd1886288
Abdulhakeem Sakhel <abodhakeemfabl@gmail.com>
2023-06-10 23:34:43 +0300

    todo 2

--- debugging.py
+++ debugging.py
@@ -1,28 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
-# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
-#          the image's filename. This function inputs: 
-#           - The Image Folder as image_dir within get_pet_labels function and 
-#             as in_arg.dir for the function call within the main function. 
-#          This function creates and returns the results dictionary as results_dic
-#          within get_pet_labels function and as results within main. 
-#          The results_dic dictionary has a 'key' that's the image filename and
-#          a 'value' that's a list. This list will contain the following item
-#          at index 0 : pet image label (string).
-#
-##
-# Imports python modules
 from os import listdir
-
-# TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
-#       with this function
-# 
 def get_pet_labels(image_dir):
     """
     Creates a dictionary of pet labels (results_dic) based upon the filenames 
@@ -43,6 +19,7 @@ def get_pet_labels(image_dir):
     # Replace None with the results_dic dictionary that you created with this
     filename_list = listdir(image_dir)
     results_dic = dict()
+
     for filename in filename_list:
         if results_dic.get(filename) is None:
             filename_labels = get_filename_labels(filename)
@@ -53,5 +30,9 @@ def get_pet_labels(image_dir):
 
     return results_dic
 
+#get_pet_labels('./pet_images/')
 def get_filename_labels(filename: str)-> list:
     return [label for label in filename.lower().strip().split('_') if label.isalpha()]
+
+dic = get_pet_labels('./pet_images/')
+print(dic)
